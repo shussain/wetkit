6a9d2c5ded933c489875cadd683d60e03a2eb0fa
Peijun Zhang <peijunz@hotmail.com>
2018-05-01 15:32:14 -0400

    nhpid migrate-d2d, modified to migrate field_title correctly

--- sites/wetkit_nhpid/modules/scripts/set_multi_valued_entity_ref_field_widget_to_autocomplate.php
+++ sites/wetkit_nhpid/modules/scripts/set_multi_valued_entity_ref_field_widget_to_autocomplate.php
@@ -1,6 +1,6 @@
 
 $types = get_all_nhpid_data_node_types();
-foeach($types as $type){
+foreach($types as $type=>$label){
 	$instances=field_info_field_instances('node', $type);
 	foreach($instances as $field_name => $settings){
 		$field=field_info_field($field_name);
